780b42504a8c8f7584b97b707284c1c799484a2c
kowalsking <kovvalsking@gmail.com>
2021-07-22 23:46:06 +0300

    Echo bot

--- bot.js
+++ bot.js
@@ -1,12 +1,22 @@
 const { Telegraf } = require('telegraf')
 
-const bot = new Telegraf('1895899439:AAGq3yivTVoApAG5NLAyddj_zUZBjbI3gxI')
+const bot = new Telegraf('1895899439:AAGq3yivTVoApAG5NLAyddj_zUZBjbI3gxI') //bujigua_bot
 
 const helpMessage = `
   Say something to me
   /start - start the bot
   /help - command reference
 `
+
+bot.use((ctx, next) => {
+  console.log(ctx.message.sticker)
+  // if (ctx.updateSubTypes[0] === 'text') {
+  //   console.log(ctx.from.username + ' said ' + ctx.message.text)
+  // } else {
+  //   console.log(ctx.from.username + ' sent ' + ctx.updateSubTypes[0]);
+  // }
+  next()
+})
 
 bot.start(ctx => {
   ctx.reply('Hello epta')
