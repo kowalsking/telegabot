16549c70c57b45b46bb56fe3622e630d18e90e40
kowalsking <kovvalsking@gmail.com>
2021-08-26 13:55:59 +0200

    Help message

--- bot.js
+++ bot.js
@@ -5,6 +5,20 @@ const bot = new Telegraf('1895899439:AAGq3yivTVoApAG5NLAyddj_zUZBjbI3gxI') //buj
 const axios = require('axios')
 const fs = require('fs')
 
+const helpMessage = 
+`
+Simple API Bot
+/fortune - get a fortune cookie
+/cat - get a random cat pic
+/cat <text> - get cat image with text
+/dogbreeds - get a list of dog breeds
+/dog <breed> - get image of dog breed
+`
+
+bot.help(ctx => {
+  ctx.reply(helpMessage)
+})
+
 bot.command('fortune', ctx => {
   axios.get('http://yerkee.com/api/fortune')
   .then(res => {
@@ -40,6 +54,43 @@ bot.command('dogbreeds', ctx => {
   })
 
   ctx.reply(message)
+})
+
+bot.command('dog', ctx => {
+  const input = ctx.message.text.split(' ')
+  if (input.length !== 2) {
+    ctx.reply('You must give a dog breed as the second argument')
+    return
+  }
+  const breedInput = input[1]
+
+  const rawdata = fs.readFileSync('dogbreeds.json', 'utf8')
+  let data = JSON.parse(rawdata)
+
+  if (data.includes(breedInput)) {
+    axios.get(`https://dog.ceo/api/breed/${breedInput}/images/random`)
+    .then(res => {
+      ctx.replyWithPhoto(res.data.message)
+    }).catch(e => {
+      console.log(e)
+    })
+  } else {
+    let suggestions = data.filter(item => {
+      return item.startsWith(breedInput)
+    })
+    let message = `Did you meand:
+`
+    suggestions.forEach(item => {
+      message += `-${item}
+`
+    })
+
+    if (suggestions.length === 0) {
+      ctx.reply('Cannot find breed')
+    } else {
+      ctx.reply(message)
+    }
+  }
 })
 
 bot.launch()
