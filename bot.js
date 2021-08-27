82f253e2c09ab373c921ac6d66e5698ea80ac185
kowalsking <kovvalsking@gmail.com>
2021-08-27 13:21:14 +0200

    Added crypto buttons

--- bot.js
+++ bot.js
@@ -13,8 +13,27 @@ bot.command('start', ctx => {
     reply_markup: {
       inline_keyboard: [
         [ { text: 'Crypto Prices', callback_data: 'price' } ],
-        [ { text: 'CoinMarketCap', url: 'https://www.cryptocompare.com/' } ],
+        [ { text: 'CoinMarketCap', url: 'https://www.cryptocompare.com/' } ]
+      ]
+    }
+  })
+})
 
+bot.action('price', ctx => {
+  const priceMessage = `Get price Information. Select one of the cryptocurrecnies below`
+  ctx.deleteMessage()
+  bot.telegram.sendMessage(ctx.chat.id, priceMessage, {
+    reply_markup: {
+      inline_keyboard: [
+        [ 
+          { text: 'BTC', callback_data: 'price-BTC' },
+          { text: 'ETH', callback_data: 'price-ETH' }
+        ],
+        [ 
+          { text: 'BCH', callback_data: 'price-BCH' },
+          { text: 'LTC', callback_data: 'price-LTC' }
+        ],
+        [ { text: 'Back to Menu', callback_data: 'start' } ]
       ]
     }
   })
