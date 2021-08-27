d1d230644076eaa0d724f29f19172a04381484ea
kowalsking <kovvalsking@gmail.com>
2021-08-27 13:10:52 +0200

    Added api test key

--- bot.js
+++ bot.js
@@ -5,48 +5,5 @@ const bot = new Telegraf('1895899439:AAGq3yivTVoApAG5NLAyddj_zUZBjbI3gxI') //buj
 const axios = require('axios')
 const fs = require('fs')
 
-bot.command('test', ctx => {
-  bot.telegram.sendMessage(ctx.chat.id, 'Menu', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'See Fruits List', callback_data: 'fruits' }
-        ],
-        [
-          { text: 'See Meats List', callback_data: 'meats' }
-        ],
-      ]
-    }
-  })
-})
-
-bot.action('fruits', ctx => {
-  ctx.deleteMessage()
-  bot.telegram.sendMessage(ctx.chat.id, 'List of fruits:\n-Apples\n-Oranges\n-Pears', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'Back to menu', callback_data: 'menu' }
-        ]
-      ]
-    }
-  })
-})
-
-bot.action('menu', ctx => {
-  ctx.deleteMessage()
-  bot.telegram.sendMessage(ctx.chat.id, 'Menu', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'See Fruits List', callback_data: 'fruits' }
-        ],
-        [
-          { text: 'See Meats List', callback_data: 'meats' }
-        ],
-      ]
-    }
-  })
-})
-
+const api_key = '7712a525b44f22e9e26ce8400ddfd6c992fbb4a24d8accdd68c4c6845581baa2' // test key
 bot.launch()
