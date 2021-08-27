adc7af22fdcdea07bdaace837c8105006722e408
kowalsking <kovvalsking@gmail.com>
2021-08-27 13:15:34 +0200

    Added 2 interactive buttons

--- bot.js
+++ bot.js
@@ -5,5 +5,19 @@ const bot = new Telegraf('1895899439:AAGq3yivTVoApAG5NLAyddj_zUZBjbI3gxI') //buj
 const axios = require('axios')
 const fs = require('fs')
 
-const api_key = '7712a525b44f22e9e26ce8400ddfd6c992fbb4a24d8accdd68c4c6845581baa2' // test key
+const apikey = '7712a525b44f22e9e26ce8400ddfd6c992fbb4a24d8accdd68c4c6845581baa2' // test key
+
+bot.command('start', ctx => {
+  let startMessage = `Welcome to cryptocurrency bot!`
+  bot.telegram.sendMessage(ctx.chat.id, startMessage, {
+    reply_markup: {
+      inline_keyboard: [
+        [ { text: 'Crypto Prices', callback_data: 'price' } ],
+        [ { text: 'CoinMarketCap', url: 'https://www.cryptocompare.com/' } ],
+
+      ]
+    }
+  })
+})
+
 bot.launch()
